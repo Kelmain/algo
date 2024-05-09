58eef41fd0e032a128b1cf1cc0ad6c1ac0496693
Kelmain <neumann.armin79@gmail.com>
2024-05-09 16:55:54 +0200

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+Trying to create visualization for the algorithms
+
---
+++ algo.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import numpy as np
+import plotly.graph_objects as go
+import matplotlib.pyplot as plotly
+
+
+
+# TODO: sorting algorithms: Bubble sort(nb), selectionn sort(nb), insertion sort(nb), merge sort(nb), quick sort(nb), heap sort(nb),
+# TODO: list search: linear search, binary search, hash search,
+# TODO: graph search: breadth-first search, depth-first search, bellman-ford, dijkstra, A* algorithm
+# TODO: tree search: BST, AVL, RB, B-Tree, Heap
+# TODO: try to creating graphs to visualize the algorithms
+
